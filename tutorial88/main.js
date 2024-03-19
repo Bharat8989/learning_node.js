675a47bf65f341a112312b7af262d3ccb3b86376
Bharat8989 <kadamb208@gmail.com>
2024-03-19 08:59:23 +0530

    adding files

--- tutorial88/main.js
+++ tutorial88/main.js
@@ -1,45 +1,54 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-// app.get or app.post or app.put or app.delete(path, handler)
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+    res.send('Hello Bharat!');
+});
 
 app.get('/about', (req, res) => {
-    res.send('About us')
-})
+    res.send('About us');
+});
 
 app.get('/contact', (req, res) => {
-    res.send('Hello contact me!')
-})
+    res.send('Hello contact me!');
+});
 
 app.get('/blog', (req, res) => {
-    res.send('Hello blog!')
-})
+    res.send('Hello blog!');
+});
 
 app.get('/blog/:slug', (req, res) => {
-    // logic to fetch {slug} from the db
-    // For URL: http://localhost:3000/blog/intro-to-padosi?mode=dark&region=in
-    console.log(req.params) // will output { slug: 'intro-to-padosi' }
-    console.log(req.query) // will output { mode: 'dark', region: 'in' }
+    console.log(req.params);
+    console.log(req.query);
 
-    res.send(`hello ${req.params.slug}`)
-})
-
-// app.get('/blog/intro-to-js', (req, res) => {
-//     // logic to fetch intro to js from the db
-//     res.send('Hello intro-to-js!')
-// })
-
-// app.get('/blog/intro-to-python', (req, res) => {
-//     // logic to fetch intro to python from the db
-//     res.send('Hello intro-to-python!')
-// })
+    res.send(`hello ${req.params.slug}`);
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
+// const http = require('node:http');
+// const hostname = '127.0.0.1';
+// const port = 3000;
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World\n');
+// });
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+// const express = require('express');
+// const app = express();
+// const port = 3000;
+
+// app.get('/', (req, res) => {
+//   res.send("hello world");
+// });
+
+// app.listen(port, () => {
+//   console.log(`example app listening on port ${port}`);
+// });
---
+++ tutorial88/tempCodeRunnerFile.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/about', (req, res) => {
+    res.send('About us');
+});
+
+app.get('/contact', (req, res) => {
+    res.send('Hello contact me!');
+});
+
+app.get('/blog', (req, res) => {
+    res.send('Hello blog!');
+});
+
+app.get('/blog/:slug', (req, res) => {
+    console.log(req.params);
+    console.log(req.query);
+
+    res.send(`hello ${req.params.slug}`);
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
+const http = require('node:http');
+const hostname = '127.0.0.1';
+const port = 3000;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
